c0bee78964e39781b94b5d1b2785ec14ed2fb1a6
John Maybee <john.d.maybee@gmail.com>
2022-12-18 18:41:31 -0500

    corrected my sql database name. Created the taskLibrary and connected it to the module that connects to pg. wrote out the router.post and router.get routes.

--- database.sql
+++ database.sql
@@ -1,6 +1,6 @@
--- first query when creating the task_table
-CREATE TABLE "task_table" (
-	"id" serial primary key,
-	"task" varchar(80) not null,
-	"status" varchar(40) not null
-);
+-- -- -- first query when creating the task_table -- -- --
+-- CREATE TABLE "task_table" (
+-- 	"id" serial primary key,
+-- 	"task" varchar(80) not null,
+-- 	"status" varchar(40) not null
+-- );
--- server/routes/taskLibrary.js
+++ server/routes/taskLibrary.js
@@ -4,21 +4,38 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+router.post('/', (req, res) => {
+    const newTask = req.body;
+    const queryText = `
+        INSERT INTO "task_table" ("task", "status")
+        VALUES ($1, $2) 
+    `;
+    pool.query(queryText, [newTask.task, newTask.status])
+    .then((result) => {
+        console.log('result', result);
+        res.sendStatus(201);
+    })
+    .catch((error) => {
+        console.log('error making insert query', error);
+        res.sendStatus(500);
+    });
+});
+
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * from task_table ORDER BY "id" asc;';
+    pool.query(queryText)
+    .then((result) => {
+        console.log('results from database', result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('error making a query', error);
+        res.sendStatus(500);
+    })
+});
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+// need to add a router.delete to use along with the delete button functionality
 
 
 module.exports = router;
