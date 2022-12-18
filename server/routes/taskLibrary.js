99e4a0c279f6cc85738a125ec4d62cff8531bebb
John Maybee <john.d.maybee@gmail.com>
2022-12-18 15:25:26 -0500

    added in a postgress database with the required name and created a table then posted the sql query into the database.sql. Also set up the pool.js, and started on the taskLibrary route

--- server/routes/taskLibrary.js
+++ server/routes/taskLibrary.js
@@ -1,3 +1,24 @@
 // need to set up a rout that connects us to the modules that connect us to our database
 // then need to send sql querys over to make our logic/code functional
+const express = require('express');
+const router = express.Router();
+const pool = require('../modules/pool');
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
