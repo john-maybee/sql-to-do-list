d4d1edc76df293d16a4ea457a11236d498eeff39
John Maybee <john.d.maybee@gmail.com>
2022-12-17 22:00:31 -0500

    added to the ajax GET request. Through this function, the table body is cleared and the new information is appended including the button. added click handlers for the new delete and comple buttons

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -8,8 +8,10 @@ function onReady() {
     // need to call the getTasks function here to start with any existing tasks
     $('#submit').on('click', postTask);
     // need to make a click event to call a function to handle delete click events
+    $('#incompleteTableBody').on('click', '.delete', handleDelete);
     // same for status button. (on click could update the status, and be able to flip it back)
-}
+    $('#incompleteTableBody').on('click', '.status', handleStatus);
+} // end of onReady function
 
 
 ///////////////////////////////////////postTask function///////////////////////////////////////
@@ -29,3 +31,31 @@ function postTask() {
         getTasks();
     });
 } // end of postTask function
+
+
+///////////////////////////////////////getTasks function///////////////////////////////////////
+
+function getTasks() {
+    $('#incompleteTableBody').empty(); // clear the table body for the top table
+    $.ajax({
+        type: 'GET',
+        url: '/taskLibrary'
+    }).then( function (response) {
+        console.log('GET /taskLibrary response', response);
+        // need to append the data to the DOM
+        for (let i = 0; i < response.length; i++) {
+            $('#incompleteTableBody').append(`
+                <tr data-id=${response[i].id}>
+                    <td>${response[i].task}</td>
+                    <td>${response[i].status}</td>
+                    <td>
+                        <button class="status-change">completed</button>
+                    </td>
+                    <td>
+                        <button class="delete">delete</button>
+                    </td>
+                </tr>
+            `)
+        }
+    })
+}
