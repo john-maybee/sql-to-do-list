2775387b9e03e1b93f086594a0bf7316ebda1b0a
John Maybee <john.d.maybee@gmail.com>
2022-12-18 20:25:17 -0500

    trying to bug fix and updated some files

--- database.sql
+++ database.sql
@@ -1,6 +1,9 @@
 -- -- -- first query when creating the task_table -- -- --
--- CREATE TABLE "task_table" (
--- 	"id" serial primary key,
--- 	"task" varchar(80) not null,
--- 	"status" varchar(40) not null
--- );
+CREATE TABLE "tasks" (
+	"id" serial primary key,
+	"task" varchar(80) not null,
+	"status" varchar(40) not null
+);
+
+INSERT INTO "tasks"
+    ("task", "status");
--- server/routes/taskLibrary.js
+++ server/routes/taskLibrary.js
@@ -4,6 +4,20 @@ const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
 
+
+router.get('/', (req, res) => {
+    let queryText = 'SELECT * from task_table ORDER BY "id" asc;';
+    pool.query(queryText)
+    .then((result) => {
+        console.log('results from database', result);
+        res.send(result.rows);
+    })
+    .catch((error) => {
+        console.log('error making a query', error);
+        res.sendStatus(500);
+    })
+});
+
 router.post('/', (req, res) => {
     const newTask = req.body;
     const queryText = `
@@ -19,19 +33,6 @@ router.post('/', (req, res) => {
         console.log('error making insert query', error);
         res.sendStatus(500);
     });
-});
-
-router.get('/', (req, res) => {
-    let queryText = 'SELECT * from task_table ORDER BY "id" asc;';
-    pool.query(queryText)
-    .then((result) => {
-        console.log('results from database', result);
-        res.send(result.rows);
-    })
-    .catch((error) => {
-        console.log('error making a query', error);
-        res.sendStatus(500);
-    })
 });
 
 
